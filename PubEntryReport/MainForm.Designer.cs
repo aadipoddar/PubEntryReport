d318449d9b43e62603e64da6fe8f729fb7b0d5e8
Aadi Poddar <aadipoddarmail@gmail.com>
2024-12-06 17:20:56 +0530

    CRUD Operation

---
+++ PubEntryReport/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PubEntryReport
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			insertButton = new Button();
+			nameTextBox = new TextBox();
+			numberTextBox = new TextBox();
+			nameLabel = new Label();
+			numberLabel = new Label();
+			personListBox = new ListBox();
+			getDataButton = new Button();
+			updateButton = new Button();
+			deleteButton = new Button();
+			SuspendLayout();
+			// 
+			// insertButton
+			// 
+			insertButton.Location = new Point(845, 205);
+			insertButton.Name = "insertButton";
+			insertButton.Size = new Size(128, 45);
+			insertButton.TabIndex = 0;
+			insertButton.Text = "Insert";
+			insertButton.UseVisualStyleBackColor = true;
+			insertButton.Click += insertButton_Click;
+			// 
+			// nameTextBox
+			// 
+			nameTextBox.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			nameTextBox.Location = new Point(899, 89);
+			nameTextBox.Name = "nameTextBox";
+			nameTextBox.Size = new Size(385, 39);
+			nameTextBox.TabIndex = 1;
+			// 
+			// numberTextBox
+			// 
+			numberTextBox.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			numberTextBox.Location = new Point(899, 151);
+			numberTextBox.Name = "numberTextBox";
+			numberTextBox.Size = new Size(385, 39);
+			numberTextBox.TabIndex = 2;
+			// 
+			// nameLabel
+			// 
+			nameLabel.AutoSize = true;
+			nameLabel.Font = new Font("Segoe UI", 18F);
+			nameLabel.Location = new Point(792, 92);
+			nameLabel.Name = "nameLabel";
+			nameLabel.Size = new Size(78, 32);
+			nameLabel.TabIndex = 3;
+			nameLabel.Text = "Name";
+			// 
+			// numberLabel
+			// 
+			numberLabel.AutoSize = true;
+			numberLabel.Font = new Font("Segoe UI", 18F);
+			numberLabel.Location = new Point(792, 154);
+			numberLabel.Name = "numberLabel";
+			numberLabel.Size = new Size(102, 32);
+			numberLabel.TabIndex = 4;
+			numberLabel.Text = "Number";
+			// 
+			// personListBox
+			// 
+			personListBox.FormattingEnabled = true;
+			personListBox.ItemHeight = 15;
+			personListBox.Location = new Point(12, 12);
+			personListBox.Name = "personListBox";
+			personListBox.Size = new Size(757, 739);
+			personListBox.TabIndex = 6;
+			personListBox.SelectedIndexChanged += personListBox_SelectedIndexChanged;
+			// 
+			// getDataButton
+			// 
+			getDataButton.Location = new Point(845, 266);
+			getDataButton.Name = "getDataButton";
+			getDataButton.Size = new Size(128, 45);
+			getDataButton.TabIndex = 7;
+			getDataButton.Text = "Get Data";
+			getDataButton.UseVisualStyleBackColor = true;
+			getDataButton.Click += getDataButton_Click;
+			// 
+			// updateButton
+			// 
+			updateButton.Location = new Point(1002, 205);
+			updateButton.Name = "updateButton";
+			updateButton.Size = new Size(128, 45);
+			updateButton.TabIndex = 8;
+			updateButton.Text = "Update";
+			updateButton.UseVisualStyleBackColor = true;
+			updateButton.Click += updateButton_Click;
+			// 
+			// deleteButton
+			// 
+			deleteButton.Location = new Point(1156, 205);
+			deleteButton.Name = "deleteButton";
+			deleteButton.Size = new Size(128, 45);
+			deleteButton.TabIndex = 9;
+			deleteButton.Text = "Delete";
+			deleteButton.UseVisualStyleBackColor = true;
+			deleteButton.Click += deleteButton_Click;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1528, 787);
+			Controls.Add(deleteButton);
+			Controls.Add(updateButton);
+			Controls.Add(getDataButton);
+			Controls.Add(personListBox);
+			Controls.Add(numberLabel);
+			Controls.Add(nameLabel);
+			Controls.Add(numberTextBox);
+			Controls.Add(nameTextBox);
+			Controls.Add(insertButton);
+			Name = "MainForm";
+			Text = "MainForm";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button insertButton;
+		private TextBox nameTextBox;
+		private TextBox numberTextBox;
+		private Label nameLabel;
+		private Label numberLabel;
+		private ListBox personListBox;
+		private Button getDataButton;
+		private Button updateButton;
+		private Button deleteButton;
+	}
+}
---
+++ PubEntryReport/MainForm.cs
@@ -0,0 +1,150 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp;
+using static PubEntryReport.MainForm;
+
+namespace PubEntryReport
+{
+	public partial class MainForm : Form
+	{
+		public class Person
+		{
+			public string Id { get; set; }
+			public string Name { get; set; }
+			public string Number { get; set; }
+		}
+
+		List<Person> persons;
+		IFirebaseConfig config = new FirebaseConfig
+		{
+			AuthSecret = "GBZsC8h9K24WuvS48u4h5Ao3ltH8EKDDlJG5ea4g",
+			BasePath = "https://pubentryreport-default-rtdb.asia-southeast1.firebasedatabase.app/"
+		};
+
+		FirebaseClient client;
+
+		public MainForm()
+		{
+			InitializeComponent();
+			client = new FirebaseClient(config);
+			persons = new List<Person>();
+			FillListBox();
+		}
+
+		private async void FillListBox()
+		{
+			persons.Clear();
+			persons = await GetData();
+
+			personListBox.Items.Clear();
+			foreach (var person in persons)
+			{
+				personListBox.Items.Add(person);
+				personListBox.DisplayMember = "Name";
+				personListBox.ValueMember = "Id";
+			}
+		}
+
+		async Task<List<Person>> GetData()
+		{
+			var per = await client.GetAsync("Persons");
+			if (per.Body != "null")
+				foreach (var VARIABLE in (await client.GetAsync("Persons")).ResultAs<Person[]>())
+					if (VARIABLE != null)
+						persons.Add(VARIABLE);
+
+			return persons;
+		}
+
+		private async void insertButton_Click(object sender, EventArgs e)
+		{
+			persons = await GetData();
+			Person person = new Person();
+			person.Name = nameTextBox.Text;
+			person.Number = numberTextBox.Text;
+
+			if (persons.Count != 0)
+				person.Id = (Convert.ToInt64(persons.LastOrDefault().Id) + 1).ToString();
+
+			await client.SetAsync($"Persons/{persons.Count}", person);
+
+			ClearForm();
+			FillListBox();
+		}
+
+		private void ClearForm()
+		{
+			nameTextBox.Text = string.Empty;
+			numberTextBox.Text = string.Empty;
+		}
+
+		private void getDataButton_Click(object sender, EventArgs e)
+		{
+			FillListBox();
+		}
+
+		private void personListBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (personListBox.SelectedIndex != -1)
+			{
+				nameTextBox.Text = persons.ElementAt(personListBox.SelectedIndex).Name.ToString();
+				numberTextBox.Text = persons.ElementAt(personListBox.SelectedIndex).Number.ToString();
+			}
+		}
+
+		private async void updateButton_Click(object sender, EventArgs e)
+		{
+			if (personListBox.SelectedIndex != -1)
+			{
+				Person person = new Person();
+				person.Name = nameTextBox.Text;
+				person.Number = numberTextBox.Text;
+				person.Id = persons.ElementAt(personListBox.SelectedIndex).Id;
+
+				int c = 0, i = 0;
+				foreach (var VARIABLE in (await client.GetAsync("Persons")).ResultAs<Person[]>())
+					c++;
+
+				for (i = 0; i < c; i++)
+				{
+					int id = 0;
+					var getId = await client.GetAsync($"Persons/{i}/Id");
+					if (getId.Body != "null")
+						id = (int)Convert.ToInt64(getId.Body.Substring(1, getId.Body.Length - 2));
+					if (id == (int)Convert.ToInt64(person.Id))
+						break;
+				}
+
+				await client.UpdateAsync($"Persons/{i}", person);
+
+				ClearForm();
+				FillListBox();
+			}
+		}
+
+		private async void deleteButton_Click(object sender, EventArgs e)
+		{
+			if (personListBox.SelectedIndex != -1)
+			{
+				int c = 0, i = 0;
+				foreach (var VARIABLE in (await client.GetAsync("Persons")).ResultAs<Person[]>())
+					c++;
+
+				for (i = 0; i < c; i++)
+				{
+					int id = 0;
+					var getId = await client.GetAsync($"Persons/{i}/Id");
+					if (getId.Body != "null")
+						id = (int)Convert.ToInt64(getId.Body.Substring(1, getId.Body.Length - 2));
+					if (id == (int)Convert.ToInt64(persons.ElementAt(personListBox.SelectedIndex).Id))
+						break;
+				}
+
+				await client.DeleteAsync($"Persons/{i}");
+			}
+
+			ClearForm();
+			FillListBox();
+		}
+	}
+}
